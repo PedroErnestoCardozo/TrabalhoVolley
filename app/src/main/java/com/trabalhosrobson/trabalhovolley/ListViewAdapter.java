a87d23eb763071d5e6771ce86e4c50fd6a4e5f15
PedroErnestoCardozo <32229879+PedroErnestoCardozo@users.noreply.github.com>
2018-05-05 20:35:47 -0300

    search

--- app/src/main/java/com/trabalhosrobson/trabalhovolley/ListViewAdapter.java
+++ app/src/main/java/com/trabalhosrobson/trabalhovolley/ListViewAdapter.java
@@ -5,11 +5,11 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Filter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import com.trabalhosrobson.trabalhovolley.UsuarioConst;
-
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -20,6 +20,7 @@ public class ListViewAdapter extends ArrayAdapter<UsuarioConst> {
 
 
     private List<UsuarioConst> userList;
+    private List<UsuarioConst> orig;
     private Context mCtx;
 
     public ListViewAdapter(List<UsuarioConst> userList, Context mCtx) {
@@ -27,6 +28,47 @@ public class ListViewAdapter extends ArrayAdapter<UsuarioConst> {
         this.userList = userList;
         this.mCtx = mCtx;
     }
+
+    @Override
+    public int getCount() {
+        return userList.size();
+    }
+
+    @Override
+    public Filter getFilter() {
+        return new Filter() {
+            @Override
+            protected FilterResults performFiltering(CharSequence constraint) {
+                final FilterResults oReturn = new FilterResults();
+                final ArrayList<UsuarioConst> results = new ArrayList<UsuarioConst>();
+                if (orig == null) {
+                    orig = userList;
+                }
+                if (constraint != null) {
+                    constraint = constraint.toString().toLowerCase();
+                    if (orig != null && orig.size() > 0) {
+                        for (final UsuarioConst g : orig) {
+                            if ((g.getNome().toLowerCase().contains(constraint.toString())) ||
+                                    (g.getSobrenome().toLowerCase().contains(constraint.toString())) ||
+                                    g.getEmail().toLowerCase().contains(constraint.toString())) {
+                                results.add(g);
+                            }
+                        }
+                    }
+                    oReturn.values = results;
+                }
+                return oReturn;
+            }
+
+            @SuppressWarnings("unchecked")
+            @Override
+            protected void publishResults(CharSequence constraint, FilterResults results) {
+                userList = (ArrayList<UsuarioConst>) results.values;
+                notifyDataSetChanged();
+            }
+        };
+    }
+
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent){
@@ -44,7 +86,7 @@ public class ListViewAdapter extends ArrayAdapter<UsuarioConst> {
 
         txtNome.setText(usuarioConst.getNome() + " " +usuarioConst.getSobrenome());
         txtEmail.setText(usuarioConst.getEmail());
-        img.setImageBitmap(usuarioConst.getImg());
+        //img.setImageBitmap(usuarioConst.getImg());
 
         return listViewItem;
     }
--- app/src/main/java/com/trabalhosrobson/trabalhovolley/MainActivity.java
+++ app/src/main/java/com/trabalhosrobson/trabalhovolley/MainActivity.java
@@ -4,8 +4,13 @@ import android.app.ProgressDialog;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ListView;
+import android.widget.SearchView;
 import android.widget.Toast;
 
 import com.android.volley.Request;
@@ -19,27 +24,42 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SearchView.OnQueryTextListener {
 
     private static String JSON_URL = "https://sistemagte.xyz/android/trabRobson/listar.php";
     ListView listView;
     List<UsuarioConst> usuarioList;
-    private int idCrianca;
     ProgressDialog progressDialog;
     RequestQueue requestQueue;
-
+    ListViewAdapter adapter;
+    SearchView searchView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         requestQueue = Volley.newRequestQueue(this);
-
         progressDialog = new ProgressDialog(MainActivity.this);
+
+
+        listView = findViewById(R.id.listView);
+        searchView = findViewById(R.id.barra_pesquisa);
+        usuarioList = new ArrayList<>();
+        PuxarDados();
+        listView.setTextFilterEnabled(true);
+        setupSearchView();
+
+    }
+
+    private void setupSearchView() {
+        searchView.setIconifiedByDefault(false);
+        searchView.setOnQueryTextListener(this);
+        searchView.setSubmitButtonEnabled(true);
+        searchView.setQueryHint("Pesquisar...");
     }
 
     public void fab(View view) {
@@ -47,9 +67,7 @@ public class MainActivity extends AppCompatActivity {
         startActivity(Tela);
     }
 
-    private void loadCriancaList() {
-        usuarioList.clear();
-
+    private void PuxarDados() {
         // Showing progress dialog at user registration time.
         progressDialog.setMessage("Carregando dados");
         progressDialog.show();
@@ -70,13 +88,12 @@ public class MainActivity extends AppCompatActivity {
                             for (int i = 0; i < jsonArray.length(); i++) {
                                 JSONObject jsonObject = jsonArray.getJSONObject(i);
                                 UsuarioConst usuarioConst = new UsuarioConst(jsonObject.getString("nome"),jsonObject.getString("sobrenome"),jsonObject.getString("email"),jsonObject.getString("foto"));
-
                                 usuarioList.add(usuarioConst);
                             }
-
-                            ListViewAdapter adapter = new ListViewAdapter(usuarioList, getApplicationContext());
+                            adapter = new ListViewAdapter(usuarioList, getApplicationContext());
 
                             listView.setAdapter(adapter);
+
                         } catch (JSONException e) {
                             e.printStackTrace();
                         }
@@ -92,23 +109,25 @@ public class MainActivity extends AppCompatActivity {
                         // Showing error message if something goes wrong.
                         Toast.makeText(MainActivity.this, volleyError.toString(), Toast.LENGTH_LONG).show();
                     }
-                }) {
-            @Override
-            protected Map<String, String> getParams() {
-
-                // Creating Map String Params.
-                Map<String, String> params = new HashMap<String, String>();
-
-                // Adding All values to Params.
-                params.put("id", "valor");
-
-                return params;
-            }
-
-        };
+                });
 
         requestQueue.getCache().clear();
         requestQueue.add(stringRequest);
+    }
+
+    @Override
+    public boolean onQueryTextChange(String newText){
+        if (TextUtils.isEmpty(newText)) {
+            listView.clearTextFilter();
+        } else {
+            listView.setFilterText(newText);
+        }
+        return true;
+    }
+
+    @Override
+    public boolean onQueryTextSubmit(String query){
+        return false;
     }
 
 }
