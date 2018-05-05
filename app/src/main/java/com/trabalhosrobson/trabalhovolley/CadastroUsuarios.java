81e0ac43abc2135c138a5b5ef3b9674da38f6369
Andreatta <mateus-henrique2001@hotmail.com>
2018-05-05 15:20:37 -0300

    cadastro

---
+++ app/src/main/java/com/trabalhosrobson/trabalhovolley/CadastroUsuarios.java
@@ -0,0 +1,13 @@
+package com.trabalhosrobson.trabalhovolley;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CadastroUsuarios extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastro_usuarios);
+    }
+}
