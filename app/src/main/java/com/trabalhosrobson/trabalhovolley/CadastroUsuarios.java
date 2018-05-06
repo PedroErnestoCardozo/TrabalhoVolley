06ab3f52d2aa4d0c5a1cb0d4e5d4991a09f43105
PedroErnestoCardozo <32229879+PedroErnestoCardozo@users.noreply.github.com>
2018-05-06 00:34:34 -0300

    cadastrando fotos

--- app/src/main/java/com/trabalhosrobson/trabalhovolley/CadastroUsuarios.java
+++ app/src/main/java/com/trabalhosrobson/trabalhovolley/CadastroUsuarios.java
@@ -9,8 +9,10 @@ import android.net.Uri;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Base64;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
 
@@ -21,6 +23,7 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
@@ -32,12 +35,26 @@ public class CadastroUsuarios extends AppCompatActivity {
     Bitmap photoBitmap;
     ImageView photoView;
 
+    EditText nome,sobrenome,email;
+
+    RequestQueue requestQueue;
+    ProgressDialog progressDialog;
+
+    String HttpUrl = "https://sistemagte.xyz/android/trabRobson/cad.php";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastro_usuarios);
+
+        requestQueue = Volley.newRequestQueue(this);
+        progressDialog = new ProgressDialog(CadastroUsuarios.this);
+
         btnFotos = findViewById(R.id.btnImg);
         photoView = findViewById(R.id.imgSelecionada);
+        nome = findViewById(R.id.nome);
+        sobrenome = findViewById(R.id.sobrenome);
+        email = findViewById(R.id.email);
 
         btnFotos.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -45,8 +62,6 @@ public class CadastroUsuarios extends AppCompatActivity {
                 Intent pickPhoto = new Intent(Intent.ACTION_PICK,
                         MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
                 startActivityForResult(pickPhoto, CODE_REQ);
-
-
 
             }
 
@@ -72,7 +87,59 @@ public class CadastroUsuarios extends AppCompatActivity {
 
 
     public void cadastrarImg(View view) {
+        progressDialog.setMessage("Cadastrando");
+        progressDialog.show();
+        // Creating string request with post method.
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, HttpUrl,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String ServerResponse) {
+                        //Toast.makeText(cad_crianca.this, ServerResponse, Toast.LENGTH_LONG).show();
+                        System.out.println(ServerResponse);
+                        Toast.makeText(CadastroUsuarios.this, ServerResponse, Toast.LENGTH_LONG).show();
+                        // Hiding the progress dialog after all task complete.
+                        progressDialog.dismiss();
 
+                        // Showing response message coming from server.
+                        Toast.makeText(CadastroUsuarios.this, "Cadastrado!", Toast.LENGTH_SHORT).show();
+                        Intent tela = new Intent(CadastroUsuarios.this, MainActivity.class);
+                        startActivity(tela);
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError volleyError) {
+
+                        progressDialog.dismiss();
+
+                        Toast.makeText(CadastroUsuarios.this, volleyError.toString(), Toast.LENGTH_LONG).show();
+                    }
+                }) {
+            @Override
+            protected Map<String, String> getParams() {
+
+                Map<String, String> params = new HashMap<>();
+
+                params.put("nome", nome.getText().toString());
+                params.put("sobrenome", sobrenome.getText().toString());
+                params.put("email", email.getText().toString());
+                params.put("foto", imageToString(photoBitmap));
+
+                return params;
+            }
+
+        };
+
+        requestQueue.getCache().clear();
+        requestQueue.add(stringRequest);
+    }
+
+    private String imageToString(Bitmap bitmap){
+
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, byteArrayOutputStream);
+        byte[] imgBytes = byteArrayOutputStream.toByteArray();
+        return Base64.encodeToString(imgBytes, Base64.DEFAULT);
     }
 
 }
