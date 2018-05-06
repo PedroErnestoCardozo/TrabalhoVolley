2e30d6f6592e49df69828d573729a88fa7b89408
PedroErnestoCardozo <32229879+PedroErnestoCardozo@users.noreply.github.com>
2018-05-05 22:19:43 -0300

    pegando img

--- app/src/main/java/com/trabalhosrobson/trabalhovolley/CadastroUsuarios.java
+++ app/src/main/java/com/trabalhosrobson/trabalhovolley/CadastroUsuarios.java
@@ -1,8 +1,17 @@
 package com.trabalhosrobson.trabalhovolley;
 
+import android.app.Activity;
 import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.android.volley.Request;
@@ -12,60 +21,58 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
 public class CadastroUsuarios extends AppCompatActivity {
 
-    RequestQueue requestQueue;
-    ProgressDialog progressDialog;
-    private static String URL_INSERT = "";
+    Button btnFotos;
+    private static int CODE_REQ = 0205;
+    Bitmap photoBitmap;
+    ImageView photoView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastro_usuarios);
+        btnFotos = findViewById(R.id.btnImg);
+        photoView = findViewById(R.id.imgSelecionada);
 
-        requestQueue = Volley.newRequestQueue(this);
-        progressDialog = new ProgressDialog(CadastroUsuarios.this);
-    }
-
-    private void EnviarDados(){
-        progressDialog.setMessage("Cadastrando");
-        progressDialog.show();
-
-        StringRequest stringRequest = new StringRequest(Request.Method.POST, URL_INSERT,
-                new Response.Listener<String>() {
-                    @Override
-                    public void onResponse(String ServerResponse) {
-                        progressDialog.dismiss();
-                        Toast.makeText(CadastroUsuarios.this, "Cadastrado com sucesso", Toast.LENGTH_SHORT).show();
-                    }
-                },
-                new Response.ErrorListener() {
-                    @Override
-                    public void onErrorResponse(VolleyError volleyError) {
-
-                        // Hiding the progress dialog after all task complete.
-                        progressDialog.dismiss();
-
-                        // Showing error message if something goes wrong.
-                        Toast.makeText(CadastroUsuarios.this, volleyError.toString(), Toast.LENGTH_LONG).show();
-                    }
-                }) {
+        btnFotos.setOnClickListener(new View.OnClickListener() {
             @Override
-            protected Map<String, String> getParams() {
+            public void onClick(View v) {
+                Intent pickPhoto = new Intent(Intent.ACTION_PICK,
+                        MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(pickPhoto, CODE_REQ);
 
-                Map<String, String> params = new HashMap<>();
 
-                params.put("id", "value");
 
-                return params;
             }
 
-        };
-
-        requestQueue.getCache().clear();
-        requestQueue.add(stringRequest);
+        });
     }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (data != null && resultCode == Activity.RESULT_OK && requestCode == CODE_REQ) {
+            Uri photoData = data.getData();
+
+            try {
+                photoBitmap = MediaStore.Images.Media.getBitmap(getContentResolver(),photoData);
+            } catch (IOException e) {
+                e.printStackTrace();
+                System.out.println(e.getMessage());
+            }
+            photoView.setImageBitmap(photoBitmap);
+        }
+
+
+    }
+
+
+    public void cadastrarImg(View view) {
+
+    }
+
 }
